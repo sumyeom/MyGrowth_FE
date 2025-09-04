a3af32daa8172eea0f823a05410f19334fdcd204
sumyeom <wjstndus75@naver.com>
2025-09-04 20:25:17 +0900

    🩹 fix : 프록시 코드 추가, 라우터 추가
    - 프록시 설정
    - 라우터에 컴포넌트 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import LoginForm from '@/components/Auth/LoginForm.vue';
+import GoalSetup from '@/components/GoalSetup/GoalSetup.vue';
+import RoutineList from '@/components/Routine/RoutineList.vue';
 
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: LoginForm },
+  { path: '/goal-setup', component: GoalSetup },
+  { path: '/routines', component: RoutineList },
 ];
 
 const router = createRouter({

30da9095e0c58bcc2553ffd82d42a36e6b130cc1
sumyeom <Qkrwlstjd!0621>
2025-09-07 00:58:27 +0900

    🐛 bug fix : 날짜 전달하는 함수 수정
    - getDay()로 되어 있어 getDate()로 수정

--- src/components/Routine/RoutineCreate.vue
+++ src/components/Routine/RoutineCreate.vue
@@ -230,7 +230,7 @@ const formattedDate = computed(() => {
     if(!date) return;
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDay()).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
     return `${year}-${month}-${day}`;
 })
 

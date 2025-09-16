05d9976d6b32da3001a3913386a5f6f6cab584a9
sumyeom <Qkrwlstjd!0621>
2025-09-16 16:24:01 +0900

    🩹 fix : swagger 추가

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,19 @@ module.exports = {
       '/api': {
         target: 'http://localhost:8081', // Spring Boot 주소
         changeOrigin: true
-      }
+      },
+      "/swagger-ui.html": {
+        target: "http://localhost:8081",
+        changeOrigin: true,
+      },
+      "/swagger-ui": {
+        target: "http://localhost:8081",
+        changeOrigin: true,
+      },
+      "/v3/api-docs": {
+        target: "http://localhost:8081",
+        changeOrigin: true,
+      },
     }
   }
 }
